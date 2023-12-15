0f6db16afe7d6443c5102ba42b2b1684889caf25
Prathmeshm1206 <prathmeshm1206@gmail.com>
2023-12-15 23:03:20 +0530

    bg-color

---
+++ Backround Color Switcher/main.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button){
+    
+
+    // callback function for event generate
+
+   button.addEventListener('click', function(e){                ///mouse event
+       if(e.target.id === 'grey')
+       {
+        body.style.backgroundColor = e.target.id;
+       }
+       if(e.target.id === 'white')
+       {
+        body.style.backgroundColor = e.target.id;
+       }
+       if(e.target.id === 'blue')
+       {
+        body.style.backgroundColor = e.target.id;
+       }
+       if(e.target.id === 'yellow')
+       {
+        body.style.backgroundColor = e.target.id;
+       }
+
+    });
+    
+});
